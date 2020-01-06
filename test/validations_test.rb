a25df4e435770958b15c80b3cb89b60b998163a1
Nick Muerdter <12112+GUI@users.noreply.github.com>
2020-01-06 12:30:27 -0700

    Fix crash when sheet name exceeds maximum length.

--- test/validations_test.rb
+++ test/validations_test.rb
@@ -24,4 +24,24 @@ describe "FastExcel validations" do
     assert_equal("Sheet1", ws1[:name])
     assert_equal("Sheet2", ws2[:name])
   end
+
+  it "should raise error when the sheet name exceeds maximum length" do
+    workbook = FastExcel.open(constant_memory: true)
+
+    error = assert_raises do
+      workbook.add_worksheet("ABCDEFGHIJKLMNOPQRSTUVWXYZ012345")
+    end
+
+    assert_equal(ArgumentError, error.class)
+    assert_equal("Worksheet name 'ABCDEFGHIJKLMNOPQRSTUVWXYZ012345' exceeds Excel's limit of 31 characters", error.message)
+  end
+
+  it "should not raise error when the sheet name is at maximum length" do
+    workbook = FastExcel.open(constant_memory: true)
+
+    worksheet = workbook.add_worksheet("ABCDEFGHIJKLMNOPQRSTUVWXYZ01234")
+    worksheet.append_row(["aaa", "bbb", "ccc"])
+
+    assert_equal("ABCDEFGHIJKLMNOPQRSTUVWXYZ01234", worksheet[:name])
+  end
 end
