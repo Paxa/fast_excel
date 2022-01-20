675ed913e6a0dca39db18e257588354de2924695
Wes Oldenbeuving <wes@narnach.com>
2022-01-20 15:09:18 +0100

    Refactor: cleanup date test
    * Test names better reflect intentions
    * Test variable naming is consistent
    * Corrected flipped actual and expected values
    
    I'm adding more tests and prefer they are all consistent. Afterwards
    there is room for another refactoring to clean up some duplicated
    wrapper code.

--- test/date_test.rb
+++ test/date_test.rb
@@ -2,28 +2,28 @@ require_relative 'test_helper'
 
 describe "FastExcel.date_num" do
 
-  it "should save correct date" do
+  it "should correct save a Date" do
     workbook = FastExcel.open("test.xlsx", constant_memory: true)
     worksheet = workbook.add_worksheet("Payments Report")
 
     date_format = workbook.number_format("[$-409]m/d/yy h:mm AM/PM;@")
     worksheet.set_column(0, 0, 20, date_format)
 
-    date = DateTime.parse('2017-03-01 15:15:15 +0000').to_time
+    value = DateTime.parse('2017-03-01 15:15:15 +0000').to_time
 
-    worksheet.write_number(0, 0, FastExcel.date_num(date), nil)
+    worksheet.write_number(0, 0, FastExcel.date_num(value), nil)
     workbook.close
 
     data = parse_xlsx_as_matrix("test.xlsx")
 
-    assert_equal(data[0][0].to_time, date)
+    assert_equal(value, data[0][0].to_time)
   end
 
 end
 
 describe "FastExcel.write_value" do
 
-  it "should save correct datetime" do
+  it "should correctly save a DateTime" do
     workbook = FastExcel.open(constant_memory: true)
     worksheet = workbook.add_worksheet
 
@@ -35,10 +35,11 @@ describe "FastExcel.write_value" do
 
     data = parse_xlsx_as_matrix(workbook.filename)
 
-    assert_equal(data[0][0], value)
+    assert_equal(value, data[0][0])
   end
 
-  it "should save correct date" do
+
+  it "should correctly save a Date" do
     workbook = FastExcel.open(constant_memory: true)
     worksheet = workbook.add_worksheet
 
@@ -50,7 +51,6 @@ describe "FastExcel.write_value" do
 
     data = parse_xlsx_as_matrix(workbook.filename)
 
-    assert_equal(data[0][0], value)
+    assert_equal(value, data[0][0])
   end
-
 end
