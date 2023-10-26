02d6b10cc9c3c63711d9cb25b978a00c9e76f147
Steffan Sluis <steffan@feedbackfruits.com>
2023-10-26 21:17:18 +0200

    auto-width: On close set empty columns to default width

--- test/auto_width_test.rb
+++ test/auto_width_test.rb
@@ -16,4 +16,24 @@ describe "FastExcel text_width" do
 
     assert_equal(sheet.calculated_column_widths, {0 => 3.52, 1 => 5.28, 2 => 14.96, 3 => 44.88})
   end
+
+  it "should set the default column width for an empty column on close" do
+    workbook = FastExcel.open(constant_memory: false)
+    sheet = workbook.add_worksheet
+    sheet.auto_width = true
+
+    sheet.append_row([
+      nil,
+      "tini",
+      "Longer",
+      "Some longer text!",
+      "This gem is FFI binding for libxlsxwriter C library"
+    ])
+
+    assert_equal(sheet.calculated_column_widths, {0 => nil, 1 => 3.52, 2 => 5.28, 3 => 14.96, 4 => 44.88})
+
+    workbook.close
+
+    assert_equal(sheet.calculated_column_widths, {0 => FastExcel::DEF_COL_WIDTH, 1 => 3.52, 2 => 5.28, 3 => 14.96, 4 => 44.88})
+  end
 end
